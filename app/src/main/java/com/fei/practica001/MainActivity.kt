1b014e1cad0dd71484221d948b2869b474041b4a
ericjair <ericjairlome@gmail.com>
2024-04-11 17:38:29 -0600

    Proyecto para el control de versiones

---
+++ app/src/main/java/com/fei/practica001/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.fei.practica001
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        setContentView(R.layout.activity_main)
+        val btn_Siguiente = findViewById<Button>(R.id.btn_siguiente)
+        btn_Siguiente.setOnClickListener {
+            val intent = Intent(this, Actividad2::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
